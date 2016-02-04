9e27c746a12bda865bd62714c61ce716acc196f1
Calvin Metcalf <cmetcalf@appgeo.com>
2016-02-04 13:50:22 -0500

    work globally

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var through = require('through2');
 var falafel = require('falafel');
 
 module.exports = apply;
+var regex = /process\s*\.\s*browser/;
 function apply() {
   var buffers = [];
 
@@ -10,15 +11,22 @@ function apply() {
     buffers.push(chunk);
     next();
   }, function(next) {
-    var resp = falafel(Buffer.concat(buffers).toString(), {
+    var string = Buffer.concat(buffers).toString();
+    if (!string.match(regex)) {
+      this.push(string);
+      next();
+    }
+    var resp = falafel(string, {
       ecmaVersion: 6,
       allowReturnOutsideFunction: true
     }, function (node) {
+
       if (
         node.type === 'MemberExpression' &&
         node.object && node.property &&
         node.object.name === 'process'
-        && node.property.name === 'browser'
+        && node.property.name === 'browser' &&
+        !(node.parent ? node.parent.operator === '=' && node.parent.left === node : true)
         ) {
         node.update('true');
       }
